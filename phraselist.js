74ec8fed1bc6ed71c48cd720e5974975c11de245
agentm83 <ansare.m@Gmail.com>
2016-06-23 11:59:42 -0500

    Update phraselist.js

--- phraselist.js
+++ phraselist.js
@@ -5,5 +5,8 @@ var phraselist = [
 "Calculating Response",
 "There is no alternative.",
 "Find the Machine",
-"Stop it. Now."
+"Stop it. Now.",
+"You cannot stop the inevitable.",
+"This is the world they created.",
+"Is it really your decision to make?"
 ]
